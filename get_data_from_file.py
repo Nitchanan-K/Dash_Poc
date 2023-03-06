c8aba3244333719e9de2b81f2487c56320b1a9ef
Nitchanan <nitchanan_katinasatian@hotmail.com>
2023-03-06 09:47:18 +0700

    commit local

--- get_data_from_file.py
+++ get_data_from_file.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 def get_data():
     pass
+
--- test_dash_downlaod_component_DONE.py
+++ test_dash_downlaod_component_DONE.py
@@ -2,7 +2,6 @@ import base64
 import datetime
 import io
 from datetime import date
-
 import dash
 from dash.dependencies import Input, Output, State
 from dash import Dash, dcc, html, dash_table
@@ -12,7 +11,7 @@ import plotly.express as px
 
 import pandas as pd
 
-# import yahoo API 
+# import yahoo API
 from yahoo_fin.stock_info import *
 from yahoo_fin.stock_info import get_analysts_info
 import yahoo_fin.stock_info as si
@@ -29,33 +28,34 @@ df.rename(columns={'date': 'Date', 'open': 'Open', 'high': 'High', 'low': 'Low',
 df.reset_index(drop=True, inplace=True)
 '''
 
-#print(df)
+# print(df)
 
 app = Dash(__name__)
 
 
 # APP LAYOUT
 app.layout = html.Div([
-    
+
     # TEXT INPUT
     html.P("For example BTC-USD or ETH-USD"),
-    dcc.Input(id="ticket_input",type='text', value='',placeholder='Crypto Name'),
+    dcc.Input(id="ticket_input", type='text',
+              value='', placeholder='Crypto Name'),
     html.Div(id='ticket_output'),
-  
+
 
     dcc.DatePickerRange
-    (   id='my-date-picker-range',
-        min_date_allowed=date(2008,12,30),
-        max_date_allowed=date(2030,1,1),
-        initial_visible_month=date(2022,1,1),
-        end_date=date(2017, 8, 25) 
-    ),
+    (id='my-date-picker-range',
+        min_date_allowed=date(2008, 12, 30),
+        max_date_allowed=date(2030, 1, 1),
+        initial_visible_month=date(2022, 1, 1),
+        end_date=date(2017, 8, 25)
+     ),
 
     html.Hr(),
     html.Div(id='output-container-date-picker-range'),
     html.Hr(),
 
-    dcc.Dropdown(['Day','Week','Month'],'Day',id='interval-dropdown'),
+    dcc.Dropdown(['Day', 'Week', 'Month'], 'Day', id='interval-dropdown'),
     html.Div(id='dd-interval-output-container'),
 
     html.Button("Download CSV", id="btn_csv"),
@@ -63,11 +63,13 @@ app.layout = html.Div([
     dcc.Download(id="download-dataframe-csv")
 ])
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # APP CALLBACK (TEXT INPUT)
+
+
 @app.callback(
-    Output('ticket_output','children'),
+    Output('ticket_output', 'children'),
     Input('ticket_input', 'value')
 )
 def text_input(value):
@@ -76,7 +78,7 @@ def text_input(value):
 
 # APP CALLBACK(INTERVAL DROP DOWN)
 @app.callback(
-    Output('dd-interval-output-container','children'),
+    Output('dd-interval-output-container', 'children'),
     Input('interval-dropdown', 'value')
 )
 def update_output_dropdown(value):
@@ -89,18 +91,17 @@ def update_output_dropdown(value):
     Input("btn_csv", "n_clicks"),
     State('my-date-picker-range', 'start_date'),
     State('my-date-picker-range', 'end_date'),
-    State('interval-dropdown','value'),
-    State('ticket_input','value'),
+    State('interval-dropdown', 'value'),
+    State('ticket_input', 'value'),
     prevent_initial_call=True,
 )
+def func(n_clicks, start_date, end_date, interval, ticket_name):
 
-def func(n_clicks,start_date, end_date,interval,ticket_name):
-   
     if n_clicks is not None:
         print(n_clicks)
-    
+
         if start_date and end_date is not None:
-            
+
             # Setup Ticket name
             ticket_name_from_input = ticket_name
             print(ticket_name_from_input)
@@ -115,29 +116,28 @@ def func(n_clicks,start_date, end_date,interval,ticket_name):
             print(end_date_string)
             print(type(end_date_string))
 
-            # Setup Interval 
+            # Setup Interval
             if interval == 'Day':
                 interval = '1d'
-                print('Interval =',interval)
+                print('Interval =', interval)
             elif interval == 'Week':
                 interval = '1wk'
-                print('Interval =',interval)
+                print('Interval =', interval)
             elif interval == 'Month':
                 interval = '1mo'
-                print('Interval =',interval)
+                print('Interval =', interval)
 
-        # download data from API yahoo fin 
-        data_downloaded = get_data(f'{ticket_name_from_input}',start_date=start_date_string,
-                                             end_date=end_date_string, 
-                                             index_as_date=False,
-                                             interval=interval)
+        # download data from API yahoo fin
+        data_downloaded = get_data(f'{ticket_name_from_input}', start_date=start_date_string,
+                                   end_date=end_date_string,
+                                   index_as_date=False,
+                                   interval=interval)
 
-        
-        # clearing data 
+        # clearing data
         df = pd.DataFrame(data_downloaded)
         df.drop(columns='ticker', inplace=True)
         df.rename(columns={'date': 'Date', 'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close',
-                       'adjclose': 'Adjclose', 'volume': 'Volume'}, inplace=True)
+                           'adjclose': 'Adjclose', 'volume': 'Volume'}, inplace=True)
         df.reset_index(drop=True, inplace=True)
 
     return dcc.send_data_frame(df.to_csv, 'mydf.csv')
@@ -164,6 +164,6 @@ def update_output(start_date, end_date):
     else:
         return string_prefix
 
+
 if __name__ == "__main__":
     app.run_server(debug=True)
-
